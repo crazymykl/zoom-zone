0fabe4ec0712efb545eaad0c61d1fbdebd0b71f7
Mike MacDonald <crazymykl@gmail.com>
2016-05-02 14:41:28 -0400

    Don't be overeager about touches

--- addon/components/zoom-zone.js
+++ addon/components/zoom-zone.js
@@ -29,7 +29,7 @@ export default Component.extend({
     const content = viewport.children('.zoom-content');
     const scale = this.get('scale');
     const start = (e) => {
-      if(normalizeEvent(e).touches || e.which === 1) { startPinch(e); }
+      if(Ember.isPresent(normalizeEvent(e).touches) || e.which === 1) { startPinch(e); }
       if(e.type === 'mousedown') { e.preventDefault(); }
     };
 
