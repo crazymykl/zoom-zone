c32989b6cde3a5c916f585c8d9da6498f5d8dba1
Matthew Madonna <mmadonna@rbmtechnologies.com>
2018-07-03 10:17:23 -0400

    ember try focused

---
+++ config/ember-try.js
@@ -0,0 +1,26 @@
+module.exports = {
+  scenarios: [
+    {
+      name: "ember-1.13.13",
+      dependencies: {
+        ember: "1.13.13"
+      }
+    },
+
+    //{
+      //name: "ember-2.18.1",
+      //allowedToFail: true,
+      //dependencies: {
+        //ember: "2.18.1"
+      //}
+    //},
+
+    //{
+      //name: "ember-3.0.0",
+      //allowedToFail: true,
+      //dependencies: {
+        //ember: "3.0.0-beta.2"
+      //}
+    //}
+  ]
+};
