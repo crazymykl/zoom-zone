8d3dbbd94c212428ae8213fc35e2330e01c0b4eb
Ryan Mark <rgmark@gmail.com>
2018-09-20 18:09:32 -0700

    Update (#1)
    Fix nested addon deps

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
 
 module.exports = {
   name: require('./package').name, 
-  included: function (app) {
-    this._super.included(app);
+  included: function(app) {
+    this.app = this._findHost();
+	
     app.import('vendor/zoom-zone.css');
+    this._super.included.apply(this, arguments);
   },
+  _findHost() {
+    let current = this;
+    let app;
+
+    // Keep iterating upward until we don't have a grandparent.
+    // Has to do this grandparent check because at some point we hit the project.
+    do {
+       app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
+    return app;
+  }
 };
